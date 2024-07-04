ba3978f7405ad39694b27dc24bc0a1ce6d308a73
toerttrg <147601395+toerttrg@users.noreply.github.com>
2024-07-04 02:01:33 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started indev2&nbsp;
+          Get started indev3&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
