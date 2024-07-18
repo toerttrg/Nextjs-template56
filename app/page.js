af971dc3f85a1b52f96b8605c58c9a2ce56b3461
toerttrg <147601395+toerttrg@users.noreply.github.com>
2024-07-18 18:31:23 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          New Get started inmainStart27 test&nbsp;
+          New Get started inmainStart27 test2&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
